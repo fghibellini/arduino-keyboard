070496ebfdb349f35bf0337efb5560262ad553b5
Filippo Ghibellini <ghibellini@cngroup.dk>
2018-08-20 23:27:13 +0200

    All keys

--- left.c
+++ left.c
@@ -52,18 +52,35 @@ void loop() {
   const int VAL_D = digitalRead(I_D);
 
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (VAL_A == LOW) {
-    Keyboard.write('4');
-  }
-  if (VAL_B == LOW) {
-    Keyboard.write('r');
-  }
-  if (VAL_C == LOW) {
-    Keyboard.write('f');
-  }
-  if (VAL_D == LOW) {
-    Keyboard.write('v');
-  }
+  if (i == 0 && VAL_A == LOW) { Keyboard.write('~'); }
+  if (i == 0 && VAL_B == LOW) { Keyboard.write('\t'); }
+  if (i == 0 && VAL_C == LOW) { Keyboard.write(' '); }
+  if (i == 0 && VAL_D == LOW) { Keyboard.write(' '); }
+
+  if (i == 1 && VAL_A == LOW) { Keyboard.write('1'); }
+  if (i == 1 && VAL_B == LOW) { Keyboard.write('q'); }
+  if (i == 1 && VAL_C == LOW) { Keyboard.write('a'); }
+  if (i == 1 && VAL_D == LOW) { Keyboard.write('z'); }
+
+  if (i == 2 && VAL_A == LOW) { Keyboard.write('2'); }
+  if (i == 2 && VAL_B == LOW) { Keyboard.write('w'); }
+  if (i == 2 && VAL_C == LOW) { Keyboard.write('s'); }
+  if (i == 2 && VAL_D == LOW) { Keyboard.write('x'); }
+
+  if (i == 3 && VAL_A == LOW) { Keyboard.write('3'); }
+  if (i == 3 && VAL_B == LOW) { Keyboard.write('e'); }
+  if (i == 3 && VAL_C == LOW) { Keyboard.write('d'); }
+  if (i == 3 && VAL_D == LOW) { Keyboard.write('c'); }
+
+  if (i == 4 && VAL_A == LOW) { Keyboard.write('4'); }
+  if (i == 4 && VAL_B == LOW) { Keyboard.write('r'); }
+  if (i == 4 && VAL_C == LOW) { Keyboard.write('f'); }
+  if (i == 4 && VAL_D == LOW) { Keyboard.write('v'); }
+
+  if (i == 5 && VAL_A == LOW) { Keyboard.write('5'); }
+  if (i == 5 && VAL_B == LOW) { Keyboard.write('t'); }
+  if (i == 5 && VAL_C == LOW) { Keyboard.write('g'); }
+  if (i == 5 && VAL_D == LOW) { Keyboard.write('b'); }
 
   i = (i + 1) % 6; 
   delay(100);
